2462ce54dad3797e13be4d894faa2c96ae6ea12d
xxx <25452644+bartoshevich@users.noreply.github.com>
2022-06-25 11:33:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let newN = '';
+    n = Math.abs(n);
+    n = n.toString();
+    let i = n.length - 1;
+    while ( i >= 0) {
+      newN = newN + `${n[i]}`;
+      i = i - 1;
+    }
+    return Number(newN);
 }
